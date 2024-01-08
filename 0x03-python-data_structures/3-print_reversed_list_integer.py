4466a4a4194edaa1e2a04aefaea6cbbc5ba6207e
Derrick Enam Azameti <azametid@gmail.com>
2024-01-08 23:25:25 +0000

    Fixed: 2-replace_in_list.py and 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=None):
-    if my_list is None or not my_list:  # Handle None and empty list cases
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None or not my_list:
         return
-    
-    reversed_list = reversed(my_list)
-    for num in reversed_list:
-        print("{:d}".format(num))
+
+    my_list.reverse()
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
