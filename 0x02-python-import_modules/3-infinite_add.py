679316a0b8efc4bab9695d1b5ecbc61300a76df3
Derrick Enam Azameti <azametid@gmail.com>
2024-01-11 00:47:45 +0000

    fixed: 101-easy_print.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
 
 if __name__ == "__main__":
-    i = 1
-    result = 0
-    arguments = len(argv) - 1
+    """Prints the addition of all arguments"""
+    import sys
 
-    if (arguments != 0):
-        while (i <= arguments):
-            result += int(argv[i])
-            i += 1
-    print("{}".format(result))
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
